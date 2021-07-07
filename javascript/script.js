61e8e5a3d9aabfa240832276b559e9f351875ef6
Natalia Malvicino <natalia.malvicino@gmail.com>
2021-07-07 19:51:34 -0300

    07/07

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('scroll', function() {
     var scroll = document.querySelector('#scrollToTop');
     scroll.classList.toggle("active", window.scrollY > 500)
@@ -8,3 +9,31 @@ function scrollToTop(){
         top:0
     })
 } 
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    //Toggle
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+
+    //Animación
+    navLinks.forEach((link, index)=>{
+        if (link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+        }
+        
+        });
+        //Animación burger
+        burger.classList.toggle('toggle');
+
+    });
+    
+
+}
+
+navSlide();
